ac4789c9702a58c99d1bb49a32a1ad00fd4567b7
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-09-07 13:23:25 -0400

    first commit

---
+++ EmployeeDirectory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeeDirectory
+//
+//  Created by Yaz Burrell on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello employee directory")
+    }
+
+
+}
+
