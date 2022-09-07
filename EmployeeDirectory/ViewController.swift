6e6d10fff5d332d2d525bbf1b25c1327c51ea308
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-09-07 13:25:03 -0400

    first commit

--- EmployeeDirectory/ViewController.swift
+++ EmployeeDirectory/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello employee directory")
+        print("Hello employee directoryy")
     }
 
 
